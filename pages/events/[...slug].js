5f8df02512e60da78fbe6f6c1a69ed867be97a1b
Jordan Holland <hollandjb@vcu.edu>
2023-03-28 21:09:57 -0400

    added files for routing

---
+++ pages/events/[...slug].js
@@ -0,0 +1,5 @@
+const FilteredEventsPage = () => {
+  return <h1>Filtered EVENTS PAGE</h1>;
+};
+
+export default FilteredEventsPage;
---
+++ pages/events/[id].js
@@ -0,0 +1,5 @@
+const EventDetailsPage = () => {
+  return <h1>EVENT DETAILS</h1>;
+};
+
+export default EventDetailsPage;
